eee590f26b78cb4c8bdd562fd5fda2c9ae5a8dac
Erlston Maison <maisonep@cardiff.ac.uk>
2023-06-01 15:35:55 +0100

    bottom nav bar

---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+import { View } from "react-native-web";
+
+export default function Home() {
+  return <View>My homepage</View>;
+}
---
+++ src/styles/colours.js
@@ -0,0 +1,17 @@
+const darkSlateGrey = "#2f494b";
+const jet = "#393835";
+const verdigris = "#64b6ac";
+const Celeste = "#c0fdfb";
+const yellowGreen = "#a1f665";
+const white = "ffffff";
+const black = "000000";
+
+export default {
+  darkSlateGrey,
+  jet,
+  verdigris,
+  Celeste,
+  yellowGreen,
+  white,
+  black,
+};
