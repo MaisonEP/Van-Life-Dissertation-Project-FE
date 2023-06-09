86fb50d0aa07899bdc8c3fb6fdbccee69cf6232c
Erlston Maison <maisonep@cardiff.ac.uk>
2023-06-09 11:17:01 +0100

    profile pic and content template fine tune

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,23 +1,89 @@
-import { View, Text, Dimensions } from "react-native";
-import { Surface, Stack } from "@react-native-material/core";
+import { View, Text, Dimensions, StyleSheet } from "react-native";
+import { TextInput, Surface, Stack, Avatar } from "@react-native-material/core";
+import colours from "../../styles/colours";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function Home({ navigation }) {
   const viewHeight = Dimensions.get("window").height / 3;
+  const postContent = [{ publisherName: "Erl", publisherLocation: "Cardiff" }];
+
   return (
     <Stack style={{ margin: 16 }} items="center" spacing={4}>
       <Surface
         elevation={6}
         category="medium"
         style={{
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
-          minHeight: viewHeight,
+          ...postStyle.container,
+          height: viewHeight,
         }}
+        testID="STACK ID"
       >
-        <Text>6</Text>
+        <View testID="this is the post div" style={postStyle.contentLayout}>
+          <View testID="display piccccccccccc" style={postStyle.displayPicture}>
+            <Avatar icon={(props) => <Icon name="account" {...props} />} />
+          </View>
+          <View
+            style={postStyle.usernameAndContent}
+            testID="dulicate %%%%%%%%%%%%%%%"
+          >
+            <View style={postStyle.UsernameContainer}>
+              <Text>
+                {postContent[0].publisherName},
+                {postContent[0].publisherLocation}
+              </Text>
+            </View>
+            <View style={postStyle.contentBody} testID="%%%%%%%%%%%%%%">
+              <View style={postStyle.contentTitle}>
+                <Text>Title</Text>
+              </View>
+              <View style={postStyle.contentText}>
+                <Text>Content</Text>
+              </View>
+              <View>
+                <Text>Likes, Comments, Share</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </Surface>
-      ;
     </Stack>
   );
 }
+
+const postStyle = StyleSheet.create({
+  container: {
+    backgroundColor: colours.Celeste + "70",
+    // justifyContent: "center",
+    width: "100%",
+    display: "flex",
+    // justifyContent: "flex-start",
+  },
+  contentLayout: {
+    flexDirection: "row",
+    height: "100%",
+    padding: 10,
+  },
+  contentBody: {
+    // backgroundColor: colours.jet,
+    flexGrow: 2,
+    // height: "100%",
+  },
+  displayPicture: { paddingRight: 20 },
+  usernameAndContent: {
+    // backgroundColor: colours.jet,
+    // flexGrow: 1,
+  },
+  UsernameContainer: {
+    // backgroundColor: colours.Celeste,
+    paddingBottom: 20,
+  },
+  contentTitle: {
+    paddingBottom: 20,
+  },
+  contentText: {
+    flexGrow: 2,
+  },
+  contentInteraction: {
+    flexGrow: 1,
+  },
+});
--- src/styles/colours.js
+++ src/styles/colours.js
@@ -5,6 +5,7 @@ const Celeste = "#c0fdfb";
 const yellowGreen = "#a1f665";
 const white = "ffffff";
 const black = "000000";
+const dirtBrown = "#f9d597";
 
 export default {
   darkSlateGrey,
@@ -14,4 +15,5 @@ export default {
   yellowGreen,
   white,
   black,
+  dirtBrown,
 };
