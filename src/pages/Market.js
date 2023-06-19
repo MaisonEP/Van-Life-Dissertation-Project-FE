339005ab8fc7671c8ce3a633d03da610c0dcb390
Erlston Maison <maisonep@cardiff.ac.uk>
2023-06-19 11:01:04 +0100

    Image accessor from device library

---
+++ src/pages/Market.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Market({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Market Page!</Text>
+    </View>
+  );
+}
